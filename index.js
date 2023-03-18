6d8e7034bd6acd36470ba6bcf0fbd84e15220670
Carlmelo <car1melo@hotmail.com>
2023-03-18 00:02:17 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(sample) {
   // Write your code here!
+  if (sample <= 400) {
+    return "This one is on me!";
+  
+ }
+
+  if (sample > 400 && sample < 2000) {
+    return "That will be twenty bucks.";
+ }
+
+  if (sample > 2000 && sample < 2500) {
+    return "I will gladly take your thirty bucks.";
+ }
+
+   else {
+    return "No can do.";
+ 
+ }
+
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+
+  return city === `NYC`? `Ok, sounds good.` : `No go.`
+
 }
 
-function switchOnCharmFromTip(){
+
+
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+
+  switch(tip){
+    case 'generous' :
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+
 }
